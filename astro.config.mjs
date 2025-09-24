7e35b07645bc58ea7e44af325987f440ac41b178
lordidiot <0xlordidiot@gmail.com>
2025-09-24 16:59:49 +0800

    Make links open in newtab

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import { oldBlogRedirects } from './redirects';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   site: 'https://idiot.sg',
@@ -16,7 +17,10 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: 'github-light'
-    }
+    },
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
+    ]
   },
 
   redirects: {
