16926f847eaea3b88c410fd345b7836500364008
lordidiot <0xlordidiot@gmail.com>
2025-09-24 14:42:08 +0800

    More work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://lordidiot.github.io',
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [mdx()],
+
+  markdown: {
+    shikiConfig: {
+      theme: 'github-light'
+    }
+  },
+
+  redirects: {
+    "/blog": "/blog/page/1"
+  },
 });
